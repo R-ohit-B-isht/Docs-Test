1e935e261c1d3d35b5b618e1ff4a99f144715cb5
R-ohit-B-isht <rbtunes0@gmail.com>
2024-04-12 01:56:28 -0700

    pull docs

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-const testDoc="Test Docs"
+const fs = require('fs');
+const path = require('path');
+const { exec } = require('child_process');
+
+// List of repositories to fetch JSDoc comments from
+const repositories = [
+    'https://github.com/R-ohit-B-isht/Test_Docs_1.git',
+    'https://github.com/R-ohit-B-isht/Test_Docs_2.git'
+];
+
+// Destination directory for the merged documentation
+const docsDir = path.join(__dirname, 'docs');
+
+async function mergeDocumentation() {
+  // Create the docs directory if it doesn't exist
+  if (!fs.existsSync(docsDir)) {
+    fs.mkdirSync(docsDir);
+  }
+
+  for (const repo of repositories) {
+    const repoName = path.basename(repo, '.git');
+    const repoDir = path.join(docsDir, repoName);
+
+    // Clone the repository
+    await exec(`git clone ${repo} ${repoDir}`);
+
+  }
+}
+
+mergeDocumentation();

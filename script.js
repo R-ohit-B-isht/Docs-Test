3d727dcff4c91b27c88bdca60950a233d6179737
R-ohit-B-isht <rbtunes0@gmail.com>
2024-04-13 09:59:08 -0700

    fix script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const { exec } = require('child_process');
 
 // List of repositories to fetch JSDoc comments from
 const repositories = [
-    'https://github.com/R-ohit-B-isht/Test_Docs_1.git',
-    'https://github.com/R-ohit-B-isht/Test_Docs_2.git'
+  'https://github.com/R-ohit-B-isht/Test_Docs_1.git',
+  'https://github.com/R-ohit-B-isht/Test_Docs_2.git'
 ];
 
 // Destination directory for the merged documentation
@@ -14,23 +14,22 @@ const docsDir = path.join(__dirname, 'docs');
 async function mergeDocumentation() {
   // Create the docs directory if it doesn't exist
   if (!fs.existsSync(docsDir)) {
-    fs.mkdirSync(docsDir);
+  fs.mkdirSync(docsDir);
   }
 
   for (const repo of repositories) {
-    const repoName = path.basename(repo, '.git');
-    const repoDir = path.join(docsDir, repoName);
+  const repoName = path.basename(repo, '.git');
+  const repoDir = path.join(docsDir, repoName, 'docs'); // Update the repoDir path
 
-    console.log(`Cloning ${repo}...`);
-    // Clone the repository
-    try {
-        await exec(`git clone ${repo} ${repoDir}`);
-        console.log(`Cloned ${repo} successfully.`);
-    } catch (error) {
-        console.error(`Failed to clone ${repo}. Error: ${error}`);
-    }
-    
-
+  // console.log(`Cloning ${repo}...`);
+  // Clone the repository
+  try {
+    await exec(`git clone ${repo} "${repoDir}" `);
+    console.log(`git clone ${repo} "${repoDir}" `);
+    console.log(`Cloned ${repo} successfully in ${repoDir}.`);
+  } catch (error) {
+    console.error(`Failed to clone ${repo}. Error: ${error}`);
+  }
   }
 }
 
